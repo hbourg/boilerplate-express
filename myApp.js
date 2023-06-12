af5505e65eedb9ceebbd9663bbb6eb2569cc8dc0
Harrison Bourg <63069356+hbourg@users.noreply.github.com>
2023-06-12 12:48:30 -0500

    res.send

--- myApp.js
+++ myApp.js
@@ -1,5 +1,7 @@
 let express = require('express');
-app.get("/", function(req, res){});
+app.get("/", function(req, res){
+    res.send("Hello Express");
+});
 let app = express();
 console.log('Hello World');
 
