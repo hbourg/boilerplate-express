520c31bd3452f6ce01e707d0bff313a6b49b09c4
Harrison Bourg <63069356+hbourg@users.noreply.github.com>
2023-06-12 13:33:47 -0500

    serve json

--- myApp.js
+++ myApp.js
@@ -5,6 +5,9 @@ app.get("/", function(req, res){
     res.sendFile(absolutePath = __dirname + "/views/index.html");
 });
 app.use("/public", express.static(__dirname + "/public"));
+app.get("/json", function(req, res){
+    res.json({"message": "Hello json"});
+});
 
 
 
